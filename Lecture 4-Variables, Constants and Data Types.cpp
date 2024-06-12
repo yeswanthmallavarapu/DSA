06a3a4077f6c17b8e19545c7a3805edcbbbda6b6
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-12 12:20:56 +0530

    Add files via upload

---
+++ Lecture 4-Variables, Constants and Data Types.cpp
@@ -0,0 +1,23 @@
+// Variables, Constants and Data Types 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	// Declare 2 variables and then assign them values of 3,5 and then add them
+	
+	int a,b;
+	
+	a=3;
+	b=5;
+	
+	//int c;
+	//c=a+b;
+	
+	//cout << c;
+	cout << a+b;
+	cout << endl;
+	cout << "a+b";
+	
+	return 0;
+}
