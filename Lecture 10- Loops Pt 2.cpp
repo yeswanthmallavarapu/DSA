c6f0b191d6d655c793223318dea786d6660f6ca5
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-12 14:08:05 +0530

    Add files via upload

---
+++ Lecture 10- Loops Pt 2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//	int i;
+//	i=1;
+////	for(i=1;   ;i++)
+////	{
+////		cout << i << " ";
+////	}
+//    //cout << i++;
+//    //cout << ++i;
+//    //cout << i++ << endl;
+//    //cout << i;
+//    cout << ++i << endl;
+//    cout << i;
+
+//int i,j,k;
+//i=0;
+//j=0;
+//k=0;
+//cout << i << " " << j << " " << k << endl;
+//j=i++;
+//cout << i << " " << j << " " << k << endl;
+//k=++i;
+
+     int i;
+     for(i=1;i<101;i++) // called the update which happens after the interation (part of a loop is completed)
+	 {
+	 	cout << i << " ";
+	 	// block which gets execur=ted everytime
+	 }
+}
+
