de9b90cc41f783b1ae534111eb1a618d46df90b3
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-12 12:32:00 +0530

    Add files via upload

---
+++ Lecture 5-Conditional Statements, If Else, Switch Case.cpp
@@ -0,0 +1,28 @@
+// Conditional Statements, If Else, Switch Case...
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a,b;
+//	int c,d;
+//	
+//	cout << "Input 4 Integers a,b,c and d";
+//	
+//	cin>>a;
+//	cin>>b;
+//	cin>>c;
+//	cin>>d;
+//	
+//	//Statement : a>b
+//	
+//	//cout<<(a>b && c>d);
+//	cout<<(a>b || c>d);
+//		
+//	// C++ interprets true as 1
+//	// C++ interprets false as 0
+//	
+//	return 0;
+//}
+
