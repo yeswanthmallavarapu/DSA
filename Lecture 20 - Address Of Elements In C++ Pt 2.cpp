6f6d31b44ce9786670ef59c64782247bc3c4cf3f
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-14 11:29:26 +0530

    Add files via upload

---
+++ Lecture 20 - Address Of Elements In C++ Pt 2.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	long long int a;
+//	int b;
+//	cout<<"The size of int is :"<<sizeof(b)<<" The size of long long int is:"<<sizeof(a)<<endl;
+//	cout<<"The address of a is "<<&a<<"The address of b is "<<&b;
+//	return 0;
+//}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int a;
+	int b;
+	float c;
+	cout<<"The size of int is :"<<sizeof(b)<<" The size of long long int is:"<<sizeof(a)<<endl;
+	cout<<"The address of c is "<<&c<<"The address of b is "<<&b;
+	return 0;
+}
