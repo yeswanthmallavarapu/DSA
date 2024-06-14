86632098c2fe55933c98d3b5bffb0437017f9a60
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-14 11:49:18 +0530

    Add files via upload

---
+++ Lecture 21 - Introduction To Pointers Pt 1.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int a;
+//	
+//	cout <<"The address of a is "<<&a;
+//	
+//	return 0;
+//}
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	float b;
+	cout <<"The address of a is "<<&a<<endl;
+	cout <<"The address of b is "<<&b<<endl;
+	return 0;
+}
