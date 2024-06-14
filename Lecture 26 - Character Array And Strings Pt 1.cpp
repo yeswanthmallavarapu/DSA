79d05ce341761fb8bafbeb62853d3ed45625d7d8
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-14 13:46:10 +0530

    Add files via upload

---
+++ Lecture 26 - Character Array And Strings Pt 1.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	char y,e,s,w,a,n,t,h;
+//	cin>>y>>e>>s>>w>>a>>n>>t>>h;
+//	cout <<"The name you have just entered is "<<y<<e<<s<<w<<a<<n<<t<<h;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char name[5];
+//	int i;
+//	for(i=0;i<5;i++) // I used a for loo[ which ran for i in[0,4
+//	{
+//		cin>>name[i];
+//	}
+//	cout << "The name you have just entered is "<<endl;
+//	for(i=0;i<5;i++)
+//	{
+//		cout<<name[i];
+//	}
+//}
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;  // over here string is a new datatype and not a name so string is similar to int, float, double
+	cin>>name;
+	cout<<name;
+}
