da633fd25f0e0c8d2c1faf2355a051d572d11a27
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-24 15:44:38 +0530

    Add files via upload

---
+++ Lecture 51 - Introduction To Classes Pt 2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+// We achieve this using something called a "class"
+// A class is a used defined datatype which can be customized we want
+
+class student
+{
+	public:
+		string name;
+		int mids;
+		float ends;
+		bool pre_abs;
+};
+class vehicle
+{
+	public:
+		string car_name;
+		string tyre_name;
+		int number_of_tyres;
+		int Number_of_seats;
+};
+int main()
+{
+	// There are a class of 30 students and each has the following data associated with him :
+	
+	student a,b,c; // here a,b,c are of type of student
+	
+	int d,e,f // here d,e,f are types of int
+	
+	vehicle hyundai, suzuki, ferrari
+	
+	// a,b,c over here are objects of the particulat class
+}
