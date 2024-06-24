5ce7e6855a20840502954840b30fc91ec0ecca28
yeswanth kumar mallavarapu <119961131+yeswanthmallavarapu@users.noreply.github.com>
2024-06-24 15:31:10 +0530

    Add files via upload

---
+++ Lecture 50 -Introduction To Classes Pt 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// There are a class of 30 students and each has the following data associated with him :
+	
+	string name;
+	int mids;
+	float ends;
+	bool pre_abs;
+	
+	string name_1;
+	int mids_1;
+	float ends_1;
+	bool pre_abs_1;
+}
